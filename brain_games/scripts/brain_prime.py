6182910a0e1cc49b438ccfb5944319c84b4a3ba9
Lancer <71792205+lancer147741@users.noreply.github.com>
2023-09-27 02:58:23 +0300

    Update brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 from brain_games.games import prime
-from brain_games import engine
+from brain_games import game_engine
 
 
 def main():
-    engine.play(prime)
+    game_engine.play(prime)
 
 
 if __name__ == '__main__':
     main()
-        
