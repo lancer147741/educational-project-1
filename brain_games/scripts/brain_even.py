59369018d769f54a880f84855d2a49825e8cbe83
lancer147741 <vip.wdwww@gmail.com>
2023-09-27 01:54:06 +0300

    fix even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.games import calc
+from brain_games.games import even
 from brain_games import engine
 
 
 def main():
-    engine.play(calc)
+    engine.play(even)
 
 
 if __name__ == '__main__':
