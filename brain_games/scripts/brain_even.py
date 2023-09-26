52058a93e7e6ca4235e45a89a35379512244a13d
Lancer <71792205+lancer147741@users.noreply.github.com>
2023-09-27 02:57:51 +0300

    Update brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 from brain_games.games import even
-from brain_games import engine
+from brain_games import game_engine
 
 
 def main():
-    engine.play(even)
+    game_engine.play(even)
 
 
 if __name__ == '__main__':
     main()
-
