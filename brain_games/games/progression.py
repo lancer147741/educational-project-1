05765664d25f4dbca6ced70a5c8fc7d7b1b1cd61
lancer147741 <vip.wdwww@gmail.com>
2023-09-27 01:58:34 +0300

    fix progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,22 +2,22 @@ import random
 
 
 DESCRIPTION = 'What number is missing in the progression?'
-PROGRESSION_LENGTH = 10
+SIZE_PROGRESSION = 10
 
 
-def get_question():
-    start = random.randint(1, 99)
-    step = random.randint(1, 11)
-    hidden_position = random.randint(0, PROGRESSION_LENGTH - 1)
-    progression = get_progression(start, step, PROGRESSION_LENGTH)
-    right_answer = progression[hidden_position]
-    progression[hidden_position] = '..'
-    question = " ".join(progression)
-    return right_answer, question
-
-
-def get_progression(start, step, length):
-    progression = []
-    for i in range(length):
-        progression.append(str(start + step * i))
+def generate_progression():
+    start = random.randint(1, 100)
+    rule = random.randint(2, 5)
+    progression = [start]
+    for i in range(SIZE_PROGRESSION):
+        progression.append(progression[i] + rule)
     return progression
+
+
+def generate_round_data():
+    progression = generate_progression()
+    random_index = random.randint(0, SIZE_PROGRESSION - 1)
+    right = progression[random_index]
+    progression[random_index] = '..'
+    question = list(map(str, progression))
+    return ' '.join(question), str(right)
