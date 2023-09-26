622c80b476a976158c24542ab61f5f527a1e0ac1
Lancer <71792205+lancer147741@users.noreply.github.com>
2023-09-27 02:59:58 +0300

    Update brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from brain_games.games import calc
-from brain_games import game_engine
+from brain_games import engine
 
 
 def main():
-    game_engine.play(calc)
+    engine.play(calc)
 
 
 if __name__ == '__main__':
