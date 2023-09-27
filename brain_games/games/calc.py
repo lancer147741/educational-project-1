f1e4094acd86e3a57bbd2ed54a707c8f0697ae2b
Lancer <71792205+lancer147741@users.noreply.github.com>
2023-09-27 03:00:26 +0300

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -18,5 +18,4 @@ def generate_round_data():
         case '+':
             right = number_a + number_b
             question = f'{number_a} + {number_b}'
-            return question, str(right)
-            
+    return question, str(right)
